6cd26a8fa63a1e82fea5b6d4bdd5a62a62b39be7
sanjanak <sunjanaazkumari@gmail.com>
2026-04-11 12:23:02 +0200

    Update app.js

--- github-user-app/app.js
+++ github-user-app/app.js
@@ -3,36 +3,72 @@ const input = document.getElementById("usernameInput");
 const button = document.getElementById("searchBtn");
 const container = document.getElementById("profileContainer");
 
+// ENTER KEY (OUTSIDE)
+input.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    button.click();
+  }
+});
+
 // 2. EVENT
 button.addEventListener("click", function () {
 
-  // 3. LOGIC
-  const username = input.value;
+  const username = input.value.trim();
+
   if (username === "") {
-    container.innerHTML = "Please enter a username";
+    container.innerHTML = "<p style='color:red;'>Please enter a username</p>";
     return;
   }
 
   const url = "https://api.github.com/users/" + username;
-    
-  container.innerHTML = "Loading...";
- 
-  fetch(url)
-  .then(res => {
-    if (!res.ok) {
-      throw new Error("User not found");
-    }
-    return res.json();
-  })
-  .then(data => {
-    container.innerHTML = `
-      <img src="${data.avatar_url}" width="100">
-      <h2>${data.login}</h2>
-      <p>Followers: ${data.followers}</p>
-    `;
-  })
-  .catch(error => {
-    container.innerHTML = "User not found";
-  });
 
-  });
+  container.innerHTML = "<p style='color:gray;'>Searching...</p>";
+
+  fetch(url)
+    .then(res => {
+      if (!res.ok) {
+        throw new Error("User not found");
+      }
+      return res.json();
+    })
+    .then(data => {
+
+      container.innerHTML = `
+  <div class="profile-card">
+    <img src="${data.avatar_url}" width="100" alt="User Avatar">
+    <h2>${data.login.charAt(0).toUpperCase() + data.login.slice(1)}</h2>
+
+    <div class="info-row">
+      <div class="info-box">
+        <p>Followers</p>
+        <strong>${data.followers}</strong>
+      </div>
+
+      <div class="info-box">
+        <p>Repos</p>
+        <strong>${data.public_repos}</strong>
+      </div>
+
+      ${data.location ? `
+      <div class="info-box">
+        <p>Location</p>
+        <strong>${data.location}</strong>
+      </div>` : ""}
+    </div>
+
+    ${data.bio ? `<p class="bio">${data.bio}</p>` : ""}
+
+    <button id="profileBtn">View GitHub Profile</button>
+  </div>
+`;
+      // BUTTON AFTER HTML CREATED
+      document.getElementById("profileBtn").addEventListener("click", function () {
+        window.open(data.html_url, "_blank");
+      });
+
+    })
+    .catch(error => {
+      container.innerHTML = "<p style='color:red;'>User not found. Please try again.</p>";
+    });
+
+});
